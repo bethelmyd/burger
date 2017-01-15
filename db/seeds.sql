f0b6ba57658330a5112ced2743bddbdb3bfd3674
David Bethelmy <bethelmyd@gmail.com>
2017-01-15 15:10:43 -0500

    added schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name, date) VALUES ('Big Mac', now());
+INSERT INTO burgers (burger_name, date) VALUES ('Whopper', now());
+INSERT INTO burgers (burger_name, date) VALUES ('Whopper Jr.', now());
