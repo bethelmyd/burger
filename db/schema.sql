4da51ecffe8426ff521ddcc8b7d49e8662ffa1c4
David Bethelmy <bethelmyd@gmail.com>
2017-01-21 22:39:09 -0500

    Set up Heroku

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP,
+	date TIMESTAMP not null default CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
